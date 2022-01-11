469fe4363afc7e18d423f8f497d88151fce8584a
The0a <theo.aracil@viacesi.fr>
2022-01-11 14:38:50 +0100

    Adding of a display app

--- RecoFaciale/main.py
+++ RecoFaciale/main.py
@@ -1,16 +1,18 @@
 import faceDetectImg
 import faceDetectVideo
 import Recognition
+import display
 
 class main:
+    display.app()
+    # print("1. Face detection (Image) \n2. Face detection (Webcam)\n3. Face recognition (Webcam)")
+    # answer = input()
+    # if answer == '1':
+    #     faceDetectImg.FaceDetectImg()
+    # elif answer == '2':
+    #     faceDetectVideo.FaceDetectVideo()
+    # elif answer == '3':
+    #     Recognition.reco_video()
 
-    print("1. Face detection (Image) \n2. Face detection (Webcam)\n3. Face recognition (Webcam)")
-    answer = input()
-    if answer == '1':
-        faceDetectImg.FaceDetectImg()
-    elif answer == '2':
-        faceDetectVideo.FaceDetectVideo()
-    elif answer == '3':
-        Recognition.reco_video()
 
 
